c3ea4ca24bb4eab66741e13a8481a9504f6ac6c1
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-25 14:30:30 +0100

    Update Skript-2019-01-14-9.ps1

--- Skript-2019-01-14-9.ps1
+++ Skript-2019-01-14-9.ps1
@@ -14,10 +14,10 @@ Get-EventLog -LogName Security | Group-Object -Property -EntryType SuccessAudit
 get-winevent -FilterHashtable @{Logname='Security';ID=4624}  -MaxEvents 10
 
 
-#list of servers to see if there is an event ID in past 24 hours, save result on a file.
-Get-Eventlog -LogName System -EntryType Error -after (Get-Date).AddHours(-24) -ID 4624 -ComputerName $server
+#Make a list of servers to see if there is an event ID in past 24 hours, save result on a file.
+Get-Eventlog -LogName System -EntryType Error -after (Get-Date).AddHours(-24) -ID 4624 -ComputerNamesamira  $server
 $results = Get-Eventlog ....
-if($results){".\script.ps1 *> C:\Temp\Logins.txt"}
+if($results){".\script.ps1 *> C:\Temp\Logsinfo.txt"}
 
 
 #get the 10 newest logs for each ID
@@ -26,3 +26,22 @@ $SecLog=Get-WinEvent -FilterHashTable @{Logname='Security';ID=4624} -Max 10
 get-eventlog security -newest 100 |
   where \{$_.entrytype -eq `
     "SuccessAudit" "FailureAudit"\}
+
+
+
+
+
+
+
+
+these are what i tried to collect from google 
+
+Get-EventLog -LogName Security -InstanceID 4624 -EntryType SuccessAudit | measure | Out-File -FilePath C:\temp\Loginfo.txt -Encoding default
+
+Get-EventLog "Security" | Where -FilterScript {$_.EventID -eq 4624 -and $_.ReplacementStrings[8] -eq 2} | Out-File -FilePath C:\temp\Loginfo.txt -Encoding default -Append
+
+Get-EventLog "Security" | Where -FilterScript {$_.EventID -eq 4624 -and $_.ReplacementStrings[8] -eq 3} | Out-File -FilePath C:\temp\Loginfo.txt -Encoding default -Append
+
+Get-EventLog "Security" | Where -FilterScript {$_.EventID -eq 4624 -and $_.ReplacementStrings[8] -eq 7} | Out-File -FilePath C:\temp\Loginfo.txt -Encoding default -Append
+
+Get-EventLog -LogName Security -InstanceID 4625 -EntryType FailureAudit | Out-File -FilePath C:\temp\Logins.txt -Encoding default -Append
